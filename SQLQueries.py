8fb3bd4449f168ec1641adac470080d6b7e07945
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-01-28 23:25:33 +0300

    исправлены ошибки и доработан код

--- SQLQueries.py
+++ SQLQueries.py
@@ -1,23 +1,23 @@
 def create_database(name):
-    COMMAND = fr'CREATE DATABASE {name};'
+    COMMAND = fr"""CREATE DATABASE {name};"""
     return COMMAND
 
 def create_table(name):
     COMMAND = fr"""CREATE TABLE {name}
                 (id int PRIMARY KEY,
                 Название nvarchar(50) UNIQUE,
-                Тип (Овощ или фрукт) nvarchar(50),
+                [Тип (Овощ или фрукт)] nvarchar(50),
                 Цвет nvarchar(30),
                 Калорийность int,
-                Краткое описание nvarchar(100)"""
+                [Краткое описание] nvarchar(100));"""
     return COMMAND
 
 def insert_data(table, columns, data):
-    COMMAND = fr"""INSERT INTO {table} {columns}
-                    VALUES
-                    {data}"""
-    return COMMAND
+    columns_str = ', '.join(columns)
+    placeholders = ', '.join(['?'] * len(data))
+    return f"INSERT INTO {table} ({columns_str}) VALUES ({placeholders})"
+
 
 def get_all_data(name):
-    COMMAND = fr"""SELECT * FROM {name}"""
+    COMMAND = fr"""SELECT * FROM {name};"""
     return COMMAND
--- simple_queries.py
+++ simple_queries.py
@@ -17,71 +17,72 @@ connection_string = f"""DRIVER={{SQL Server}};
                         SERVER={SERVER};
                         DATABASE={PAD_DATABASE};
                         UID={USER};
-                        PWD={PASSWORD}"""
+                        PWD={PASSWORD};"""
 
-# """Создание базы данных"""
-# created_db = 'FruitsAndVegetables'
-# conn = pyodbc.connect(connection_string)
-# conn.autocommit = True
-#
-# try:
-#     SQL_QUERY = SQLQueries.create_database(created_db)
-#     conn.execute(SQL_QUERY)
-# except pyodbc.ProgrammingError as ex:
-#     print(ex)
-# else:
-#     print(f'База данных {created_db} успешно создана!')
-# finally:
-#     conn.close()
+"""Создание базы данных"""
+created_db = 'FruitsAndVegetables'
+conn = pyodbc.connect(connection_string)
+conn.autocommit = True
 
-# """Создание таблицы"""
-# conn = pyodbc.connect(connection_string)
-# conn.autocommit = True
-# cursor = conn.cursor()
-# activ_db_name = 'FruitsAndVegetables'
-# table_name = 'FruitsAndVegetables'
-#
-# try:
-#     SQL_QUERY = SQLQueries.create_table(table_name)
-#     cursor.execute(fr'USE {activ_db_name};')
-#     cursor.execute(SQL_QUERY)
-# except pyodbc.ProgrammingError as ex:
-#     print(ex)
-# else:
-#     print(f'Таблица {table_name} успешно создана!')
-# finally:
-#     conn.close()
+try:
+    SQL_QUERY = SQLQueries.create_database(created_db)
+    conn.execute(SQL_QUERY)
+except pyodbc.ProgrammingError as ex:
+    print(ex)
+else:
+    print(f'База данных {created_db} успешно создана!')
+finally:
+    conn.close()
 
-# """Заполнение таблицы данными"""
-#
-# conn = pyodbc.connect(connection_string)
-# conn.autocommit = True
-# cursor = conn.cursor()
-# activ_db_name = 'FruitsAndVegetables'
-# table_name = 'FruitsAndVegetables'
-# all_data = [(1, 'Яблоко', 'Фрукт', 'Зеленый', 52, 'Сладкий фрукт с хрустящей мякотью'),
-#             (2, 'Банан', 'Фрукт', 'Желтый', 89, 'Мягкий сладкий фрукт'),
-#             (3, 'Морковь', 'Овощ', 'Оранжевый', 41, 'Хрустящий корнеплод'),
-#             (4, 'Огурец', 'Овощ', 'Зеленый', 15, 'Сочный овощ с высоким содержанием воды'),
-#             (5, 'Апельсин', 'Фрукт', 'Оранжевый', 47, 'Цитрусовый фрукт, много витамина С'),
-#             (6, 'Картофель', 'Овощ', 'Коричневый', 77, 'Крахмалистый клубень'),
-#             (7, 'Виноград', 'Фрукт', 'Зеленый', 69, 'Сладкие маленькие ягоды'),
-#             (8, 'Брокколи', 'Овощ', 'Зеленый', 34, 'Овощ из семейства капустных'),
-#             (9, 'Клубника', 'Фрукт', 'Красный', 32, 'Сладкая ароматная ягода'),
-#             (10, 'Помидор', 'Овощ', 'Красный', 18, 'Плод из семейства пасленовых')]
-# try:
-#     SQL_QUERY = SQLQueries.insert_data(table_name)
-#     cursor.execute(fr'USE {activ_db_name};')
-#     for data in all_data:
-#         cursor.execute(SQLQueries.insert_data(table_name, '(id, Название, Тип (Овощ или фрукт), Цвет, Калорийность, Краткое описание)', data))
-# except pyodbc.ProgrammingError as ex:
-#     print(ex)
-# except pyodbc.IntegrityError as er:
-#     print(er)
-# else:
-#     print(f'Таблица {table_name} успешно заполнена!')
-# finally:
-#     conn.close()
+"""Создание таблицы"""
+conn = pyodbc.connect(connection_string)
+conn.autocommit = True
+cursor = conn.cursor()
+activ_db_name = 'FruitsAndVegetables'
+table_name = 'FruitsAndVegetables'
+
+try:
+    SQL_QUERY = SQLQueries.create_table(table_name)
+    cursor.execute(fr'USE {activ_db_name};')
+    cursor.execute(SQL_QUERY)
+except pyodbc.ProgrammingError as ex:
+    print(ex)
+else:
+    print(f'Таблица {table_name} успешно создана!')
+finally:
+    conn.close()
+
+"""Заполнение таблицы данными"""
+
+conn = pyodbc.connect(connection_string)
+conn.autocommit = True
+cursor = conn.cursor()
+activ_db_name = 'FruitsAndVegetables'
+table_name = 'FruitsAndVegetables'
+columns = ['id', 'Название', '[Тип (Овощ или фрукт)]', 'Цвет', 'Калорийность', '[Краткое описание]']
+all_data = [(1, 'Яблоко', 'Фрукт', 'Зеленый', 52, 'Сладкий фрукт с хрустящей мякотью'),
+            (2, 'Банан', 'Фрукт', 'Желтый', 89, 'Мягкий сладкий фрукт'),
+            (3, 'Морковь', 'Овощ', 'Оранжевый', 41, 'Хрустящий корнеплод'),
+            (4, 'Огурец', 'Овощ', 'Зеленый', 15, 'Сочный овощ с высоким содержанием воды'),
+            (5, 'Апельсин', 'Фрукт', 'Оранжевый', 47, 'Цитрусовый фрукт, много витамина С'),
+            (6, 'Картофель', 'Овощ', 'Коричневый', 77, 'Крахмалистый клубень'),
+            (7, 'Виноград', 'Фрукт', 'Зеленый', 69, 'Сладкие маленькие ягоды'),
+            (8, 'Брокколи', 'Овощ', 'Зеленый', 34, 'Овощ из семейства капустных'),
+            (9, 'Клубника', 'Фрукт', 'Красный', 32, 'Сладкая ароматная ягода'),
+            (10, 'Помидор', 'Овощ', 'Красный', 18, 'Плод из семейства пасленовых')]
+try:
+    cursor.execute(fr'USE {activ_db_name};')
+    for data in all_data:
+        SQL_query = SQLQueries.insert_data(table_name, columns, data)
+        cursor.execute(SQL_query, data)
+except pyodbc.ProgrammingError as ex:
+    print(ex)
+except pyodbc.IntegrityError as er:
+    print(er)
+else:
+    print(f'Таблица {table_name} успешно заполнена!')
+finally:
+    conn.close()
 
 """Вывод данных из БД"""
 conn = pyodbc.connect(connection_string)
